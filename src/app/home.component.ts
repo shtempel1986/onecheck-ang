7229dd0ac3bdd0dd35fe4ecb31d988b6f5b12d0f
shtempel <o_gredi@mail.ru>
2018-07-10 14:50:41 +0300

    Улучшения #1 сделать главную страницу с ссылками

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,10 +1,12 @@
 import {Component} from "@angular/core";
+import {CalendarModel} from "./models/calendar.model";
 
 @Component({
   selector:'home-component',
-  templateUrl:'./home.component.html'
+  templateUrl:'./home.component.html',
+  styleUrls: ['./home.component.sass']
 })
 export class HomeComponent{
-  constructor(){
+  constructor(public calendar: CalendarModel){
   }
 }
